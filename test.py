6d63a6b636f5c2ec11f023bea9a45f75735497a1
server248 <qilei.li@outlook.com>
2019-03-15 17:40:00 +0800

    up

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def test(
     # Configure run
     data_cfg_dict = parse_data_cfg(data_cfg)
     nC = int(data_cfg_dict['classes'])  # number of classes (80 for COCO)
-    test_path = data_cfg_dict['val']
+    test_path = data_cfg_dict['valid']
 
     # Initialize model
     model = Darknet(cfg, img_size)
